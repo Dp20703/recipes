ab139bab9ed185be9ed3fbb550796372f61a83a6
dp <codewithdp2073@gmail.com>
2025-04-28 17:48:47 +0530

    Refactor App component to use React Router and add Home page with Header, HomeBanner, and Sidebar components; implement recipe fetching and display in ReciepCard component.

---
+++ src/pages/Home.js
@@ -0,0 +1,225 @@
+import React, { useEffect, useState } from 'react'
+import Header from '../common/Header'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <HomeBanner />
+            <SideBar />
+            {/* <Func /> */}
+        </>
+    )
+}
+
+function HomeBanner() {
+    return (
+        <>
+            <div className="uk-container">
+                <div className="uk-border-rounded-large uk-background-top-center uk-background-cover 
+    uk-background-norepeat uk-light uk-inline uk-overflow-hidden uk-width-1-1" style={{ backgroundImage: 'url(img/header.jpg)' }}>
+                    <div className="uk-position-cover uk-header-overlay" />
+                    <div className="uk-position-relative" data-uk-grid>
+                        <div className="uk-width-1-2@m uk-flex uk-flex-middle">
+                            <div className="uk-padding-large uk-padding-remove-right">
+                                <h1 className="uk-heading-small uk-margin-remove-top">Choose from thousands of recipes</h1>
+                                <p className="uk-text-secondary">Appropriately integrate technically sound value with scalable infomediaries
+                                    negotiate sustainable strategic theme areas</p>
+                                <a className="uk-text-secondary uk-text-600 uk-text-small hvr-forward" href="sign-up.html">Sign up today<span className="uk-margin-small-left" data-uk-icon="arrow-right" /></a>
+                            </div>
+                        </div>
+                        <div className="uk-width-expand@m">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+function SideBar() {
+    return (
+        <>
+            <div className="uk-section uk-section-default">
+                <div className="uk-container">
+                    <div data-uk-grid>
+                        <div className="uk-width-1-4@m sticky-container">
+                            <div data-uk-sticky="offset: 100; bottom: true; media: @m;">
+                                <h2>Recipes</h2>
+                                <ul className="uk-nav-default uk-nav-parent-icon uk-nav-filter uk-margin-medium-top" data-uk-nav>
+                                    <li className="uk-parent uk-open">
+                                        <a href="#">Dish Type</a>
+                                        <ul className="uk-nav-sub">
+                                            <li><a href="#">Appetizers &amp; Snacks</a></li>
+                                            <li><a href="#">Bread Recipes</a></li>
+                                            <li><a href="#">Cake Recipes</a></li>
+                                            <li><a href="#">Candy and Fudge</a></li>
+                                            <li><a href="#">Casserole Recipes</a></li>
+                                            <li><a href="#">Christmas Cookies</a></li>
+                                            <li><a href="#">Cocktail Recipes</a></li>
+                                            <li><a href="#">Main Dishes</a></li>
+                                            <li><a href="#">Pasta Recipes</a></li>
+                                            <li><a href="#">Pie Recipes</a></li>
+                                            <li><a href="#">Sandwiches</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="uk-parent">
+                                        <a href="#">Meal Type</a>
+                                        <ul className="uk-nav-sub">
+                                            <li><a href="#">Breakfast and Brunch</a></li>
+                                            <li><a href="#">Desserts</a></li>
+                                            <li><a href="#">Dinners</a></li>
+                                            <li><a href="#">Lunch</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="uk-parent">
+                                        <a href="#">Diet and Health</a>
+                                        <ul className="uk-nav-sub">
+                                            <li><a href="#">Diabetic</a></li>
+                                            <li><a href="#">Gluten Free</a></li>
+                                            <li><a href="#">High Fiber Recipes</a></li>
+                                            <li><a href="#">Low Calorie</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="uk-parent">
+                                        <a href="#">World Cuisine</a>
+                                        <ul className="uk-nav-sub">
+                                            <li><a href="#">Chinese</a></li>
+                                            <li><a href="#">Indian</a></li>
+                                            <li><a href="#">Italian</a></li>
+                                            <li><a href="#">Mexican</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="uk-parent">
+                                        <a href="#">Seasonal</a>
+                                        <ul className="uk-nav-sub">
+                                            <li><a href="#">Baby Shower</a></li>
+                                            <li><a href="#">Birthday</a></li>
+                                            <li><a href="#">Christmas</a></li>
+                                            <li><a href="#">Halloween</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <ReciepCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+function ReciepCard() {
+    const [data, setData] = useState([]);
+    function fetchData() {
+        fetch("https://dummyjson.com/recipes").then((res) => res.json()).then((data) => setData(data.recipes))
+
+    }
+    useEffect(() => fetchData(),
+        []
+    )
+    // console.log(data);
+    return (
+        <div className="uk-width-expand@m">
+            <div data-uk-grid>
+                <div className="uk-width-expand@m">
+                    <form className="uk-search uk-search-default uk-width-1-1">
+                        <span data-uk-search-icon />
+                        <input className="uk-search-input uk-text-small uk-border-rounded uk-form-large" type="search" placeholder="Search for recipes..." />
+                    </form>
+                </div>
+                <div className="uk-width-1-3@m uk-text-right@m uk-light">
+                    <select className="uk-select uk-select-light uk-width-auto uk-border-pill uk-select-primary">
+                        <option>Sort by: Latest</option>
+                        <option>Sort by: Top Rated</option>
+                        <option>Sort by: Trending</option>
+                    </select>
+                </div>
+            </div>
+            <div className="uk-child-width-1-2 uk-child-width-1-3@s" data-uk-grid>
+                {
+                    data.map((data, index) => {
+                        return <>
+                            <div key={index} className="uk-card">
+                                <div className="uk-card-media-top uk-inline uk-light">
+                                    <img className="uk-border-rounded-medium" src={data.image} />
+                                    <div className="uk-position-cover uk-card-overlay uk-border-rounded-medium" />
+                                    <div className="uk-position-xsmall uk-position-top-right">
+                                        <a href="#" className="uk-icon-button uk-like uk-position-z-index uk-position-relative" data-uk-icon="heart" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <h3 className="uk-card-title uk-text-500 uk-margin-small-bottom uk-margin-top">
+                                        {data.name}</h3>
+                                    <div className="uk-text-xsmall uk-text-muted" data-uk-grid>
+                                        <div className="uk-width-auto uk-flex uk-flex-middle">
+                                            <span className="uk-rating-filled" data-uk-icon="icon: star; ratio: 0.7" />
+                                            <span className="uk-margin-xsmall-left">{data.rating}</span>
+                                            <span>({data.reviewCount})</span>
+                                        </div>
+                                        <div className="uk-width-expand uk-text-right">by John Keller</div>
+                                    </div>
+                                </div>
+                                <a href="recipe.html" className="uk-position-cover" />
+                            </div>
+                        </>
+                    })
+                }
+            </div>
+            <div className="uk-margin-large-top uk-text-small">
+                <ul className="uk-pagination uk-flex-center uk-text-500 uk-margin-remove" data-uk-margin>
+                    <li><a href="#"><span data-uk-pagination-previous /></a></li>
+                    <li><a href="#">1</a></li>
+                    <li className="uk-active"><span>2</span></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#"><span data-uk-pagination-next /></a></li>
+                </ul>
+            </div>
+        </div>
+    );
+
+}
+
+// function Func() {
+//     return (
+//         <>
+//             <div id="offcanvas" data-uk-offcanvas="flip: true; overlay: true">
+//                 <div className="uk-offcanvas-bar">
+//                     <a className="uk-logo" href="index.html">Kocina</a>
+//                     <button className="uk-offcanvas-close" type="button" data-uk-close="ratio: 1.2" />
+//                     <ul className="uk-nav uk-nav-primary uk-nav-offcanvas uk-margin-medium-top uk-text-center">
+//                         <li className="uk-active"><a href="index.html">Home</a></li>
+//                         <li><a href="recipe.html">Recipe</a></li>
+//                         <li><a href="search.html">Search</a></li>
+//                         <li><a href="contact.html">Contact</a></li>
+//                         <li><a href="sign-in.html">Sign In</a></li>
+//                         <li><a href="sign-up.html">Sign Up</a></li>
+//                     </ul>
+//                     <div className="uk-margin-medium-top">
+//                         <a className="uk-button uk-width-1-1 uk-button-primary" href="sign-up.html">Sign Up</a>
+//                     </div>
+//                     <div className="uk-margin-medium-top uk-text-center">
+//                         <div data-uk-grid className="uk-child-width-auto uk-grid-small uk-flex-center">
+//                             <div>
+//                                 <a href="https://twitter.com/" data-uk-icon="icon: twitter" className="uk-icon-link" target="_blank" />
+//                             </div>
+//                             <div>
+//                                 <a href="https://www.facebook.com/" data-uk-icon="icon: facebook" className="uk-icon-link" target="_blank" />
+//                             </div>
+//                             <div>
+//                                 <a href="https://www.instagram.com/" data-uk-icon="icon: instagram" className="uk-icon-link" target="_blank" />
+//                             </div>
+//                             <div>
+//                                 <a href="https://vimeo.com/" data-uk-icon="icon: vimeo" className="uk-icon-link" target="_blank" />
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+
+//         </>
+//     )
+// }
+export default Home
